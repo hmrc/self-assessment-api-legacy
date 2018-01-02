018ffcc5e2e9c9a81037219a5a1d7ed60d5d37b1
Rakesh Avasarala <rakesh.avasarala@mtdapi>
2018-01-02 18:21:52 +0000

    [RA] MTDSA-1311: Update the RAML documentation for missing businessEntertainmentCosts expense type

--- app/uk/gov/hmrc/selfassessmentapi/resources/wrappers/SelfEmploymentPeriodResponse.scala
+++ app/uk/gov/hmrc/selfassessmentapi/resources/wrappers/SelfEmploymentPeriodResponse.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -73,4 +73,3 @@ case class SelfEmploymentPeriodResponse(underlying: HttpResponse) extends Respon
     json.asOpt[DesError].exists(_.code == DesErrorCode.INVALID_BUSINESSID)
 
 }
-

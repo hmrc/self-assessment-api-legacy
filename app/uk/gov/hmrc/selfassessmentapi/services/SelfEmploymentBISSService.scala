697e1ecd258bf3f65441f1974bfe06c95b80e609
Narendra Vijayarao <26090262+narendravijayarao@users.noreply.github.com>
2018-06-08 12:42:52 +0100

    Added self employment BISS service with self employment ID validation

---
+++ app/uk/gov/hmrc/selfassessmentapi/services/SelfEmploymentBISSService.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.selfassessmentapi.services
+
+import uk.gov.hmrc.domain.Nino
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.selfassessmentapi.connectors.SelfEmploymentBISSConnector
+import uk.gov.hmrc.selfassessmentapi.httpparsers.SelfEmploymentBISSHttpParser.SelfEmploymentBISSOutcome
+import uk.gov.hmrc.selfassessmentapi.models.{Errors, TaxYear}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+object SelfEmploymentBISSService extends SelfEmploymentBISSService {
+  override val connector = SelfEmploymentBISSConnector
+}
+
+trait SelfEmploymentBISSService {
+
+  val connector: SelfEmploymentBISSConnector
+
+  def getSummary(nino: Nino,
+                 taxYear: TaxYear,
+                 selfEmploymentId: String)
+                (implicit hc: HeaderCarrier,
+                 ec: ExecutionContext): Future[SelfEmploymentBISSOutcome] = {
+
+    val selfEmploymentPattern = "^X[A-Z0-9]{1}IS[0-9]{11}$"
+
+    if (selfEmploymentId.matches(selfEmploymentPattern)) {
+      connector.getSummary(nino, taxYear, selfEmploymentId)
+    } else {
+      Future.successful(Left(Errors.SelfEmploymentIDInvalid))
+    }
+  }
+}
---
+++ test/uk/gov/hmrc/selfassessmentapi/services/SelfEmploymentBISSServiceSpec.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.selfassessmentapi.services
+
+import org.scalatest.mockito.MockitoSugar
+import uk.gov.hmrc.domain.Nino
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.selfassessmentapi.UnitSpec
+import uk.gov.hmrc.selfassessmentapi.connectors.SelfEmploymentBISSConnector
+import uk.gov.hmrc.selfassessmentapi.fixtures.selfemployment.SelfEmploymentBISSFixture
+import uk.gov.hmrc.selfassessmentapi.mocks.connectors.MockSelfEmploymentBISSConnector
+import uk.gov.hmrc.selfassessmentapi.models.{Errors, TaxYear}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class SelfEmploymentBISSServiceSpec extends UnitSpec with MockitoSugar with MockSelfEmploymentBISSConnector {
+
+  implicit val hc: HeaderCarrier = HeaderCarrier()
+
+  val target = new SelfEmploymentBISSService {
+    override val connector: SelfEmploymentBISSConnector = mockSelfEmploymentBISSConnector
+  }
+
+  "Calling .getSummary" when {
+
+    "self employment ID is invalid" should {
+      "return a SELF_EMPLOYMENT_ID_INVALID error" in {
+        val expected = Left(Errors.SelfEmploymentIDInvalid)
+
+        val result = target.getSummary(Nino("AA123456A"), TaxYear("2017-18"), "invalidID")
+
+        await(result) shouldBe expected
+      }
+    }
+
+    "self employment ID is valid" should {
+      "return a valid response" in {
+
+        val expected = Right(SelfEmploymentBISSFixture.selfEmploymentBISS)
+
+        MockSelfEmploymentBISSConnector.getSummary(Nino("AA123456A"), TaxYear("2017-18"), "XKIS00000000988")
+          .returns(Future.successful(expected))
+
+        val result = target.getSummary(Nino("AA123456A"), TaxYear("2017-18"), "XKIS00000000988")
+
+        await(result) shouldBe expected
+      }
+    }
+
+  }
+}

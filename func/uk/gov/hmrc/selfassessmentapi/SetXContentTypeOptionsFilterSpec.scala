ebd867f1b080b7e327ff21362597b3c43dd184d0
Narendra Vijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2021-06-10 08:50:22 +0100

    removed redundant tests

--- func/uk/gov/hmrc/selfassessmentapi/SetXContentTypeOptionsFilterSpec.scala
+++ func/uk/gov/hmrc/selfassessmentapi/SetXContentTypeOptionsFilterSpec.scala
@@ -24,32 +24,6 @@ class SetXContentTypeOptionsFilterSpec extends BaseFunctionalSpec {
 
   "SetXContentTypeOptionsFilter  filter should" should {
 
-    "be applied when returning an HTTP 201 e.g.: creating a self-employment" in {
-      given()
-        .userIsSubscribedToMtdFor(nino)
-        .clientIsFullyAuthorisedForTheResource
-        .des().selfEmployment.willBeCreatedFor(nino)
-        .when()
-        .post(Jsons.SelfEmployment())
-        .to(s"/ni/${nino.nino}/self-employments")
-        .thenAssertThat()
-        .statusIs(201)
-        .responseContainsHeader(SetXContentTypeOptionsFilter.xContentTypeOptionsHeader, "nosniff".r)
-    }
-
-    "be applied when returning an HTTP 409 e.g.: attempting to create a properties business more than once" in {
-      given()
-        .userIsSubscribedToMtdFor(nino)
-        .clientIsFullyAuthorisedForTheResource
-        .des().properties.willConflict(nino)
-        .when()
-        .post(Jsons.Properties()).to(s"/ni/${nino.nino}/uk-properties")
-        .thenAssertThat()
-        .statusIs(409)
-        .responseContainsHeader(SetXContentTypeOptionsFilter.xContentTypeOptionsHeader, "nosniff".r)
-    }
-
-
     "be applied when returning an HTTP 406 without accept header e.g.: creating a self-employment" in {
       given()
         .userIsSubscribedToMtdFor(nino)
